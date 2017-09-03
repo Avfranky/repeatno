a91c69cd75fd6d4a3d365dbb2540ffbc064cadf1
Avfranky <avudai1608@gmail.com>
2017-09-03 17:16:14 +0530

    Add files via upload

--- order.java
+++ order.java
@@ -8,39 +8,19 @@ public class order {
         Scanner sc=new Scanner(System.in);
         String s=sc.nextLine();
         String[] s1=s.split(" ");
-        int a,b;
         String temp;
         for(int i=0;i<s1.length;i++)
         {
             for(int j=i+1;j<s1.length;j++)
             {
-                a=first(Integer.parseInt(s1[i]));
-                b=first(Integer.parseInt(s1[j]));
-                if(a<b)
+                if(Integer.parseInt(s1[i]+s1[j])<Integer.parseInt(s1[j]+s1[i]))
                 {
                     temp=s1[i];
                     s1[i]=s1[j];
                     s1[j]=temp;
                 }
-                if(a==b)
-                {
-                    if(Integer.parseInt(s1[i])<Integer.parseInt(s1[j]))
-                    {
-                        temp=s1[i];
-                        s1[i]=s1[j];
-                        s1[j]=temp;
-                    }
-                }
             }
             System.out.print(s1[i]);
         }        
-    }
-    public static int first(int a)
-    {
-        while(a/10!=0)
-        {
-            a/=10;
-        }
-        return a;
     }
 }
